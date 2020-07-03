9d192588cc208684282667d50f98a2376cf5394b
GersiDe <59378601+GersiDe@users.noreply.github.com>
2020-07-03 03:26:55 +0100

    fixed api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -29,6 +30,10 @@ app.use("/users", usersRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
