fc3139b1105b43595d98b09fe635849319803d9e
GersiDe <59378601+GersiDe@users.noreply.github.com>
2020-07-03 02:48:51 +0100

    fixed server file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri || process.env.MONGODB_URI, {
+mongoose.connect(uri, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
